2d131ad88c04ea933dfb471118309870fc9d5a1d
nnaumovi <70389095+nnaumovi@users.noreply.github.com>
2023-01-21 23:37:38 -0500

    Update dbsetup.py

--- database/dbsetup.py
+++ database/dbsetup.py
@@ -10,12 +10,16 @@ def create_table():
 
         delete = "DROP TABLE IF EXISTS foods"
         delete2 = "DROP TABLE IF EXISTS food_ingredients"
+        delete3 = "DROP TABLE IF EXISTS appliances"
         create_query = "CREATE TABLE foods (id INT PRIMARY KEY, name STRING, sellable INT, selling_price INT, perishable INT, time_to_spoil INT, known INT)"
-        create_query2 = "CREATE TABLE food_ingredients (id INT PRIMARY KEY, ingredient1 INT, ingredient2 INT, ingredient3 INT, height INT)"
+        create_query2 = "CREATE TABLE food_ingredients (id INT PRIMARY KEY, ingredient1 INT, ingredient2 INT, ingredient3 INT, height INT, appliance INT)"
+        create_query3 = "CREATE TABLE appliances (appliance INT PRIMARY KEY, name STRING)"
         cursor.execute(delete)
         cursor.execute(delete2)
+        cursor.execute(delete3)
         cursor.execute(create_query)
         cursor.execute(create_query2)
+        cursor.execute(create_query3)
 
         connection.commit()
         print("Table successfully created! :D")
@@ -29,25 +33,36 @@ def insert_foods(id, name, sellable, selling_price, perishable, time_to_spoil, k
                                                                               str(perishable), str(time_to_spoil),
                                                                                                         str(known))
     cursor.execute(insert_query)
+    connection.commit()
 
 def delete_foods(id):
     delete_query = "DELETE FROM foods where id=%s" %(str(id))
     cursor.execute(delete_query)
+    connection.commit()
 
-def insert_ingredients(id, ingredient1, ingredient2, ingredient3, height):
+def insert_ingredients(id, ingredient1, ingredient2, ingredient3, height, appliance):
     delete_ingredients(id)
-    insert_query = "INSERT INTO food_ingredients(id, ingredient1, ingredient2, ingredient3, height) VALUES (%s, %s, %s, %s, %s )" \
-                   % (str(id), str(ingredient1), str(ingredient2), str(ingredient3), str(height))
+    insert_query = "INSERT INTO food_ingredients(id, ingredient1, ingredient2, ingredient3, height, appliance) VALUES (%s, %s, %s, %s, %s, %s )" \
+                   % (str(id), str(ingredient1), str(ingredient2), str(ingredient3), str(height), str(appliance))
     cursor.execute(insert_query)
+    connection.commit()
 
 def delete_ingredients(id):
     delete_query = "DELETE FROM food_ingredients where id=%s" % (str(id))
     cursor.execute(delete_query)
+    connection.commit()
+
+def insert_appliance(id, name):
+    insert_query = "INSERT INTO appliances(appliance, name) VALUES ('%s', '%s')" \
+                   % (str(id), str(name))
+    cursor.execute(insert_query)
+    connection.commit()
 
 def display():
-    select_query = "SELECT * FROM food_ingredients"
+    select_query = "SELECT * FROM foods"
     cursor.execute(select_query)
     print(cursor.fetchall())
+    connection.commit()
 
 def select_recipe(ingredient1, ingredient2):
     array = [0, ingredient1, ingredient2]
@@ -56,11 +71,11 @@ def select_recipe(ingredient1, ingredient2):
                    % (str(array[0]), str(array[1]), str(array[2]))
     cursor.execute(select_query)
     print(cursor.fetchall())
+    return cursor.fetchall
 
 
 create_table()
-insert_foods(1, "pan", 0, 0, 0, 0, 1)
-insert_foods(2, "oven", 0, 0, 0, 0, 1)
+
 insert_foods(3, "wheat", 1, 1, 1, 30, 1)
 insert_foods(4, "sugarcane", 1, 1, 1, 30, 1)
 insert_foods(5, "corn", 1, 1, 1, 30, 1)
@@ -75,18 +90,21 @@ insert_foods(13, "flour", 1, 1, 1, 30, 1)
 insert_foods(14, "corn flour", 1, 1, 1, 30, 1)
 insert_foods(15, "cornbread ", 1, 1, 1, 30, 1)
 
-insert_ingredients(1, 0, 0, 0, 1)
-insert_ingredients(2, 0, 0, 0, 1)
-insert_ingredients(3, 0, 0, 0, 1)
-insert_ingredients(4, 0, 0, 0, 1)
-insert_ingredients(5, 0, 0, 0, 1)
-insert_ingredients(6, 0, 0, 0, 1)
-insert_ingredients(7, 0, 11, 13, 2)
-insert_ingredients(8, 0, 1, 6, 2)
-insert_ingredients(9, 0, 12, 13, 2)
-insert_ingredients(10, 0, 8, 9, 3)
-insert_ingredients(11, 0, 0, 0, 1)
-insert_ingredients(12, 0, 0, 0, 1)
-insert_ingredients(13, 0, 0, 0, 1)
-insert_ingredients(14, 0, 0, 0, 1)
-insert_ingredients(15, 0, 11, 14, 2)
+
+insert_ingredients(3, 0, 0, 0, 1, 0)
+insert_ingredients(4, 0, 0, 0, 1, 0)
+insert_ingredients(5, 0, 0, 0, 1, 0)
+insert_ingredients(6, 0, 0, 0, 1, 0)
+insert_ingredients(7, 0, 11, 13, 2, 2)
+insert_ingredients(8, 0, 6, 6, 2, 1)
+insert_ingredients(9, 0, 12, 13, 2, 2)
+insert_ingredients(10, 0, 8, 9, 3, 2)
+insert_ingredients(11, 0, 0, 0, 1, 0)
+insert_ingredients(12, 0, 0, 0, 1, 0)
+insert_ingredients(13, 0, 3, 3, 1, 3)
+insert_ingredients(14, 0, 5, 5, 1, 3)
+insert_ingredients(15, 0, 11, 14, 2, 2)
+
+insert_appliance(1, "pan")
+insert_appliance(2, "pot")
+insert_appliance(3, "mixing bowl")
